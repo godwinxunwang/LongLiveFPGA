a3b20e1ffb355fba0f453fc5e171c39b8b628279
Godwin Wang <xunwang@nyu.edu>
2016-11-28 18:55:28 -0500

    Finished a few operations in ALU

--- FinalProject/ALU.vhd
+++ FinalProject/ALU.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL; 
-use IEEE.NUMERIC_STD.ALL
+use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
 	port(
@@ -34,14 +34,15 @@ begin
 					when x"13" => 
 						alu_out <= op1 OR op2; -- not sure ??
 					when x"14" => 
-						alu_out <= NOT( op1 OR op2); 
+						alu_out <= NOT(op1 OR op2); 
 				end case; 
 			-- J-type instruction -- 
-			elsif(opcode = "001100") then 
+			elsif(opcode = "001100") then -- PC = {(PC + 4)[31:28], address, 00}
+				-- Should PC be part of the ALU? 
+				
 			
 			
-			
-			elsif(opcode = "111111") then 
+			elsif(opcode = "111111") then -- Halt
 			
 			
 			
@@ -57,19 +58,27 @@ begin
 					when 4 => -- ori
 						alu_out <= op1 OR op2; 
 					when 5 => -- shl
-						alu_out <= 
+						alu_out <= std_logic_vector(unsigned(op1) sll unsigned(op2)); 
 					when 6 => -- shr
-					
+						alu_out <= std_logic_vector(unsigned(op1) srl unsigned(op2)); 
 					when 7 => -- lw
-					
+
 					when 8 => -- sw
 					
 					when 9 => -- blt
 					
 					when 10 => -- beq
-					
+						if(op1 = op2) then
+							alu_out <= 1; 
+						else
+							alu_out <= 0; 
+						end if; 
 					when 11 => -- bne
-					
+						if(op1 = op2) then 
+							alu_out <= 0; 
+						else 
+							alu_out <= 1; 
+						end if; 
 				end case; 
 			end if; 
 		end if; 
