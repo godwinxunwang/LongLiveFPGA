3155765700b89a62d82b25747c14c202e3c885a7
ooobread <lin_wei1@163.com>
2016-12-04 14:00:53 -0500

    Compare

---
+++ Compare.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Compare is
+port (rs: in STD_LOGIC_VECTOR(31 downto 0);
+		rt: in STD_LOGIC_VECTOR(31 downto 0);
+		branch: in STD_LOGIC_VECTOR(1 downto 0);
+		isBranch: out STD_LOGIC);
+end Compare;
+
+architecture Behavioral of Compare is
+
+begin
+process(branch, rs, rt) begin
+  case branch is
+    when "01" => if (rs < rt) then isBranch <= '1'; else isBranch <= '0'; end if;
+	 when "10" => if (rs > rt) then isBranch <= '1'; else isBranch <= '0'; end if;
+	 when "11" => if (rs = rt) then isBranch <= '1'; else isBranch <= '0'; end if;
+	 when OTHERS => isBranch <= '0';
+  end case;
+end process;
+end Behavioral;
