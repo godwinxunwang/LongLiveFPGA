1ab7060c38b21075ef225ce4a4459dfcf0cd6d70
Godwin Wang <xunwang@nyu.edu>
2016-12-03 19:01:01 -0500

    Remove CPU.vhd

--- FinalProject/top.vhd
+++ FinalProject/top.vhd
@@ -1,8 +1,12 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_UNSIGNED.ALL; 
+use ieee.numeric_std.all;
 
 entity top is
+	port(
+		clk: in std_logic
+	); 
 end top;
 
 architecture Behavioral of top is
